09d6c14cb052c2af967c2b98016090f2169d657c
Emma <e.e.gill@outlook.com>
2022-05-11 11:47:45 +0100

    The magic of CSS variables!

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 //DEFINING VARIABLES
-let userName = document.querySelector("#player-name");
-let h2 = document.querySelector(`h2`);
-let buttonPress = document.querySelector("#fight-button");
-let playerPokemonName = document.querySelector("#pokemon-name");
-let playerChoice ;
-let computerChoice ;
+let playerNameInput = document.querySelector("#player-name");
+let welcomeHeader = document.querySelector(`#player-welcome`);
+let fightButton = document.querySelector("#fight-button");
+let pokemonNameInput = document.querySelector("#pokemon-name");
+let playerPokemon ;
+let computerPokemon ;
 
 const score = {roundCount: 0, winCount: 0, lossCount: 0, drawCount: 0};
 
@@ -14,25 +14,24 @@ const lossLi = document.querySelector('#losses');
 const drawLi = document.querySelector('#draws');
 
 //EVENT LISTENERS - 
-userName.addEventListener(`keydown`, enterName);
-playerPokemonName.addEventListener(`keydown`, handlePlayerPokemon);
-buttonPress.addEventListener(`click`, pokemonFight);
+playerNameInput.addEventListener(`keydown`, enterName);
+pokemonNameInput.addEventListener(`keydown`, handlePlayerPokemon);
+fightButton.addEventListener(`click`, pokemonFight);
 
 //FUNCTIONS
 //Player name, posts to webpage
 function enterName(e){ 
     if (e.key == `Enter`){
-    h2.innerText = `welcome  ${userName.value}  Are you ready to Fight? `;
+    welcomeHeader.innerText = `Welcome  ${playerNameInput.value}, are you ready to fight? `;
     }
 }
 
-//Player's choice of pokemon, assigns to playerChoice variable
+//Player's choice of pokemon, assigns to playerPokemon variable
 function handlePlayerPokemon(e){ 
     if (e.key == `Enter`){
-    playerChoice = playerPokemonName.value;
+    playerPokemon = pokemonNameInput.value;
     }   
 }
-
     
 
 //Computer's choice of pokemon, picked by random number 1-898
@@ -48,18 +47,18 @@ async function getPokemonData(pokeRef) {
     let pokeURL = 'https://pokeapi.co/api/v2/pokemon/' + pokeRef;
     const response = await fetch(pokeURL);
     let data = await response.json(); 
-    console.log(data.stats[1].base_stat);
+    console.log(data); //.stats[1].base_stat
     return data.stats[1].base_stat;
     }
 
 
 //Calculating the battle result
  async function pokemonFight() {
-    computerChoice = getComputerMove();
-    console.log('Player Selection:', playerChoice);
-    console.log('Computer selection:', computerChoice);
-    let playerPokemon = await getPokemonData(playerChoice);
-    let computerPokemon = await getPokemonData(computerChoice);
+    computerPokemon = getComputerMove();
+    console.log('Player Selection:', playerPokemon);
+    console.log('Computer selection:', computerPokemon);
+    let playerPokemon = await getPokemonData(playerPokemon);
+    let computerPokemon = await getPokemonData(computerPokemon);
     console.log('Player Pokemon Strength:', playerPokemon);
     console.log('Computer Pokemon Strength:', computerPokemon);
 
